1e6db8f152a9125795976dd8f23a216089394b2b
AVJ <AVJ@vijay-db88f03ad>
2016-11-10 23:18:51 -0800

    hello change some data

--- GitCheckExample/src/com/getcheck/HelloGitcheck.java
+++ GitCheckExample/src/com/getcheck/HelloGitcheck.java
@@ -5,6 +5,11 @@ public class HelloGitcheck
 	public static void main(String[] args)
 	{
 		System.out.println("hello get check saved data");
+		
+		int a=10;
+		int sum, b=20;
+		System.out.println(a+b);
+		
 	}
 
 }
